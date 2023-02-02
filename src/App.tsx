be46aa58b0902b4df8d69ab6f90296d67b8b1774
AShamshin <albert.shamshin@mail.ru>
2023-02-02 15:00:35 +0300

    layout , grid)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuqSUWGefeLef35q2txrO4W5gaQgjIrJoVvw&usqp=CAU'
+          alt=''
+        />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href=''> Profile</a>
+        </div>
+        <div>
+          <a href=''>Messages</a>
+        </div>
+        <div>
+          <a href=''>News</a>
+        </div>
+        <div>
+          <a href=''>Music</a>
+        </div>
+        <div>
+          <a href=''>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img
+            src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+            alt=''
+          />
+        </div>
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
