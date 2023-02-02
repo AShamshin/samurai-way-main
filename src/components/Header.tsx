2d1da4b1bba7d0c603a22858f772613fb7701c8f
AShamshin <albert.shamshin@mail.ru>
2023-02-02 15:21:45 +0300

    Breakdown into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className='header'>
+      <img
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuqSUWGefeLef35q2txrO4W5gaQgjIrJoVvw&usqp=CAU'
+        alt=''
+      />
+    </header>
+  );
+}
+
+export default Header;
