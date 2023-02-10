f547cbffb074dbe345d5620bab3266f7a7e9a0ef
AShamshin <albert.shamshin@mail.ru>
2023-02-10 17:23:54 +0300

    21 - Typesetting the Dialogue Page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,6 +1,22 @@
 import s from './Dialogs.module.css';
 
 const Dialogs = (props: any) => {
-  return <div className={s.content}>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItem}>
+        <div className={s.dialog + ' ' + s.active}>Алик</div>
+        <div className={s.dialog}>Серя</div>
+        <div className={s.dialog}>Дашка</div>
+        <div className={s.dialog}>Кудрявый</div>
+        <div className={s.dialog}>Витёк</div>
+        <div className={s.dialog}>Ванёк</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How is your it-kamasutra ?</div>
+        <div className={s.message}>How are you ?</div>
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
