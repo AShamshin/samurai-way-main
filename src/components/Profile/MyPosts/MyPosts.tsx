3f68e43ff2cd44ef18af62499ca517d82b61897c
AShamshin <albert.shamshin@mail.ru>
2023-02-17 15:00:54 +0300

    31. Уроки React JS - onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { ChangeEvent } from 'react';
 import { PostType } from '../../../redux/state';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
@@ -10,15 +12,21 @@ function MyPosts(props: ProfilePageType) {
   let postsElement = props.posts.map((p) => (
     <Post message={p.message} likesCount={p.likesCount} />
   ));
+  let newPostElement = React.createRef<HTMLTextAreaElement>();
+  let addPost = () => {
+    let text = newPostElement.current?.value;
+
+    return console.log(text);
+  };
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
-        <button>Add post</button>
+        <button onClick={() => addPost()}>Add post</button>
         <button>Remove</button>
       </div>
       <div className={s.posts}>{postsElement}</div>
