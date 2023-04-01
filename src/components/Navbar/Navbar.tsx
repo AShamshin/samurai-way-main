32ac1e578ef8008c3aa0a539768fa02527c3bb68
AShamshin <albert.shamshin@mail.ru>
2023-04-01 11:38:17 +0300

    53 - классовый компонент (extends React.Component)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,22 +10,29 @@ function Navbar() {
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='dialogs' activeClassName={s.activeLink}>
+        <NavLink to='/dialogs' activeClassName={s.activeLink}>
           Messages
         </NavLink>
       </div>
+
       <div className={s.item}>
-        <NavLink to='news' activeClassName={s.activeLink}>
+        <NavLink to='/users' activeClassName={s.activeLink}>
+          Users
+        </NavLink>
+      </div>
+
+      <div className={s.item}>
+        <NavLink to='/news' activeClassName={s.activeLink}>
           News
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='music' activeClassName={s.activeLink}>
+        <NavLink to='/music' activeClassName={s.activeLink}>
           Music
         </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to='settings' activeClassName={s.activeLink}>
+        <NavLink to='/settings' activeClassName={s.activeLink}>
           Settings
         </NavLink>
       </div>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,93 +1,76 @@
+import React from 'react';
 import axios from 'axios';
 import s from './users.module.css';
 import { UsersPropsType } from './UsersContainer';
 import userPhoto from '../../assets/images/user.png';
 
-let Users = (props: any) => {
-  if (props.usersPage.users.length === 0) {
+class Users extends React.Component<UsersPropsType> {
+  constructor(props: UsersPropsType) {
+    super(props);
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users')
       .then((response) => {
-        props.setUsers(response.data.items);
+        this.props.setUsers(response.data.items);
       });
-
-    // props.setUsers([
-    //   {
-    //     id: 1,
-    //     photoUrl:
-    //       'https://cs.pikabu.ru/post_img/2014/01/15/10/1389803464_232888874.jpg',
-    //     followed: false,
-    //     fullName: 'Серя',
-    //     status: 'I am a Boss',
-    //     location: { city: 'Mich', country: 'Russia' },
-    //   },
-    //   {
-    //     id: 2,
-    //     photoUrl:
-    //       'https://cs.pikabu.ru/post_img/2014/01/15/10/1389803464_232888874.jpg',
-    //     followed: true,
-    //     fullName: 'Кудрявый',
-    //     status: 'FUTURAMMA',
-    //     location: { city: 'Omicron Persei 8', country: 'Russia' },
-    //   },
-    //   {
-    //     id: 3,
-    //     photoUrl:
-    //       'https://cs.pikabu.ru/post_img/2014/01/15/10/1389803464_232888874.jpg',
-    //     followed: false,
-    //     fullName: 'Алик',
-    //     status: 'Hello World',
-    //     location: { city: 'Mich', country: 'Russia' },
-    //   },
-    // ]);
   }
 
-  return (
-    <div>
-      {props.usersPage.users.map((u: any) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={u.photos.small !== null ? u.photos.small : userPhoto}
-                alt=''
-                className={s.userPhoto}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+  // detUsers = () => {
+  //   if (this.props.usersPage.users.length === 0) {
+  //     axios
+  //       .get('https://social-network.samuraijs.com/api/1.0/users')
+  //       .then((response) => {
+  //         this.props.setUsers(response.data.items);
+  //       });
+  //   }
+  // };
+  render() {
+    return (
+      <div>
+        {this.props.usersPage.users.map((u: any) => (
+          <div key={u.id}>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div>
+                <img
+                  src={u.photos.small !== null ? u.photos.small : userPhoto}
+                  alt=''
+                  className={s.userPhoto}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              <div>{'u.location.country'}</div>
-              <div>{'u.location.city'}</div>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{'u.location.country'}</div>
+                <div>{'u.location.city'}</div>
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
-<div></div>;
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
 export default Users;
